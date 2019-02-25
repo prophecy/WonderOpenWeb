dc12679c8ec0b307e8e4f318420d32e216780454
adawat <prophecyx@hotmail.com>
2019-02-25 16:02:31 +0700

    Can get start spinner by "long press" gesture

--- SparkAR/DartGame/scripts/dartboardHandler.js
+++ SparkAR/DartGame/scripts/dartboardHandler.js
@@ -5,35 +5,60 @@
 //This source code is licensed under the license found in the
 //LICENSE file in the root directory of this source tree.
 
+// Reference:
+// https://developers.facebook.com/docs/ar-studio/reference/classes/reactivemodule.rotation/
+
 const Scene = require('Scene');
 const Reactive = require('Reactive');
 const TouchGestures = require('TouchGestures')
+const Animation = require('Animation');
+const Diagnostics = require('Diagnostics')
 
-
-var mug_ctrl = Scene.root.find('dartboard')
+var dartboard = Scene.root.find('dartboard')
 var planeTracker = Scene.root.find('planeTracker0');
-
 
 TouchGestures.onTap().subscribe(function(gesture) {
 	planeTracker.trackPoint(gesture.location);
 });
 
-TouchGestures.onPan(planeTracker).subscribe(function(gesture) {
-	planeTracker.trackPoint(gesture.location, gesture.state);
+TouchGestures.onLongPress().subscribe(function(gesture) {
+
+	// Start the animation
+	var rotation_signal = Animation.animate(time_driver, rotation_sampler);
+	time_driver.start();
+
+	dartboard.transform.rotation = rotation_signal;
 });
 
-TouchGestures.onPinch().subscribe(function(gesture) {
-	var lastScaleX = mug_ctrl.transform.scaleX.lastValue;
-	mug_ctrl.transform.scaleX = Reactive.mul(lastScaleX, gesture.scale);
+// Construct a Rotation object from a quaternion-based values.
+function axisRotation(axis_x, axis_y, axis_z, angle_degrees) {
+    var norm = Math.sqrt(axis_x*axis_x + axis_y*axis_y + axis_z*axis_z);
+    axis_x /= norm;
+    axis_y /= norm;
+    axis_z /= norm;
+    var angle_radians = angle_degrees * Math.PI / 180.0;
+    var cos = Math.cos(angle_radians/2);
+    var sin = Math.sin(angle_radians/2);
+    return Reactive.rotation(
+        cos, axis_x*sin, axis_y*sin, axis_z*sin);
+}
 
-	var lastScaleY = mug_ctrl.transform.scaleY.lastValue;
-	mug_ctrl.transform.scaleY = Reactive.mul(lastScaleY, gesture.scale);
-
-	var lastScaleZ = mug_ctrl.transform.scaleZ.lastValue;
-	mug_ctrl.transform.scaleZ = Reactive.mul(lastScaleZ, gesture.scale);
+var time_driver = Animation.timeDriver({
+    durationMilliseconds: 2000,
+    loopCount: Infinity
 });
 
-TouchGestures.onRotate(mug_ctrl).subscribe(function(gesture) {
-  var lastRotationY = mug_ctrl.transform.rotationY.lastValue;
-  mug_ctrl.transform.rotationY = Reactive.add(lastRotationY, Reactive.mul(-1, gesture.rotation));
+// Create a rotation sampler using Rotation objects generated
+// by the previously-defined axisRotation() method.
+var rotation_sampler = Animation.samplers.polyline({
+    keyframes: [
+		axisRotation(1,0,0,0),
+		axisRotation(0,0,1,90),
+		axisRotation(0,0,1,180),
+		axisRotation(0,0,1,270),
+		axisRotation(0,0,1,356)
+    ],
+    knots: [
+		0, 2, 4, 6, 8
+    ]
 });
