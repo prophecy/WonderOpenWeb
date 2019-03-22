dc3259a8953b4811e7ec0e9b464c6191be9e7b1a
adawat <prophecyx@hotmail.com>
2019-03-22 15:11:58 +0700

    Added BurningAds project

---
+++ SparkAR/BurningAds/scripts/script.js
@@ -0,0 +1,53 @@
+/*
+Welcome to scripting in Spark AR Studio!
+From making things move to triggering audio clips,
+reactive programming can help you achieve your scripting goals.
+
+Helpful links:
+
+Scripting Basics - https://fb.me/spark-scripting-basics
+Reactive Programming - https://fb.me/spark-reactive-programming
+Changelogs - https://fb.me/spark-changelog
+
+Note: Feel free to delete the contents of this script and start from scratch.
+*/
+
+// How to load in modules, complete list - https://fb.me/spark-scripting-reference
+
+//const Scene = require('Scene');
+//const Materials = require('Materials');
+
+const Diagnostics = require('Diagnostics');
+//const Persistence = require('Persistence');
+
+//var plane0 = Scene.root.find('plane0');
+
+//var waveVideoMat = Materials.get('waveVideoMat');
+//var smokeVideoMat = Materials.get('smokeVideoMat');
+
+//plane0.material = smokeVideoMat;
+
+/*
+Persistence.userScope.get('game_data').then(function(value) {
+
+    Diagnostics.log(value);
+    
+    // Todo: Set material #2
+
+
+}).catch(function(err) {
+
+    Diagnostics.log('has err!');
+    Diagnostics.log(err);
+    // If no value, can check from this error message, but it's too bloat
+    // There is no value for the requested key.
+
+    Persistence.userScope.set('game_data', {head: 'head'}).then(function(value) {
+
+        Diagnostics.log('write done!');
+    });
+
+    // Todo: Set material #1
+  
+});
+/**/
