d11b6c6ed437ee658280dcb23484eba39a825f13
Adawat Chanchua <prophecyx@hotmail.com>
2019-04-01 14:00:08 +0700

    Refactor calling function

---
+++ HighPerformanceAR/js.src/featureMatching.js
@@ -0,0 +1,26 @@
+// ------------------------------------------------------------------------
+// ------------------------------------------------------------------------
+// Video canvas
+// ------------------------------------------------------------------------
+// ------------------------------------------------------------------------
+
+var featureMatching = {
+
+    imageCanvas: undefined,
+    mediaCanvas: undefined,
+    imagePath: undefined,
+
+    imageObject: undefined,
+
+    start: function() {
+
+        //imageCanvas.wi
+        //imageObject = new Image(), colsObject, rowsObject,
+        imageObject = new Image();
+    },
+
+    generateMatchData: function() {
+
+        // Todo: Generate image
+    }
+};
