9652133d40e28bf6d49bc790b5dea13890760a23
adawat <prophecyx@hotmail.com>
2019-04-24 14:35:30 +0700

    Has image tracking callback

--- SparkAR/Shinkansen/scripts/trainHandler.js
+++ SparkAR/Shinkansen/scripts/trainHandler.js
@@ -1,46 +1,71 @@
 
 const Scene = require('Scene');
-const Reactive = require('Reactive');
 const Animation = require('Animation');
 const Diagnostics = require('Diagnostics');
 const Networking = require('Networking');
 const Patches = require('Patches');
 
-var train = Scene.root.find('plane_01');
+var targetPlane = Scene.root.find('planeTracker0');
 
-// Set visibility
-Patches.setBooleanValue("trainSceneVisible", true);
+// Track target image
+// Ref: https://developers.facebook.com/docs/ar-studio/docs/visual-programming/javascript-to-patch-bridging/
+var confidenceSub = targetPlane.confidence.monitor().subscribe(function (e) {
+
+	Diagnostics.log("e.newValue: " + e.newValue);
+
+	if (e.newValue == 'HIGH') {
+		confidenceSub.unsubscribe();
+
+		// Update the Script to Editor signal named "targetFound
+		//Patches.setPulseValue("targetFound", Reactive.once());
+		targetFound();
+	}
+});
+
+// Target found event, things start here
+function targetFound() {
+
+	startTrainScene();
+}
 
 //==============================================================================
 // Animate the plane's horizontal position continuously
 //==============================================================================
 
-// Create a set of time driver parameters
-const timeDriverParameters = {
+function startTrainScene() {
+	
+	var train = Scene.root.find('plane_01');
 
-    // The duration of the driver
-    durationMilliseconds: 7500,
-
-    // The number of iterations before the driver stops
-    loopCount: Infinity,
-
-    // Should the driver 'yoyo' back and forth
-    mirror: false
-};
-
-// Create a time driver using the parameters
-const timeDriver = Animation.timeDriver(timeDriverParameters);
-
-// Create a sampler with a quadratic change in and out from -5 to 5
-const quadraticSampler = Animation.samplers.easeInOutQuad(-900, 900);
-
-// Create an animation combining the driver and sampler
-const translationAnimation = Animation.animate(timeDriver, quadraticSampler);
-
-train.transform.x = translationAnimation;
-
-// Start the time driver (unlike value drivers this needs to be done explicitly)
-timeDriver.start();
+	// Set visibility
+	Patches.setBooleanValue("trainSceneVisible", true);
+	
+	// Create a set of time driver parameters
+	const timeDriverParameters = {
+	
+		// The duration of the driver
+		durationMilliseconds: 7500,
+	
+		// The number of iterations before the driver stops
+		loopCount: Infinity,
+	
+		// Should the driver 'yoyo' back and forth
+		mirror: false
+	};
+	
+	// Create a time driver using the parameters
+	const timeDriver = Animation.timeDriver(timeDriverParameters);
+	
+	// Create a sampler with a quadratic change in and out from -5 to 5
+	const quadraticSampler = Animation.samplers.easeInOutQuad(-900, 900);
+	
+	// Create an animation combining the driver and sampler
+	const translationAnimation = Animation.animate(timeDriver, quadraticSampler);
+	
+	train.transform.x = translationAnimation;
+	
+	// Start the time driver (unlike value drivers this needs to be done explicitly)
+	timeDriver.start();		
+}
 
 //==============================================================================
 // Session ID handler
@@ -69,10 +94,6 @@ function getRandomId() {
 	};
 	
 	Diagnostics.log("request: " + JSON.stringify(request));
-	
-	//==============================================================================
-	// Send the request and log the results
-	//==============================================================================
 	
 	// Send the request to the url
 	Networking.fetch(randomId, request).then(function(result) {
