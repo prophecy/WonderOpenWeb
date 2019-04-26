6dc675f1c89372d9d9691898d0d025afe010dc24
adawat <prophecyx@hotmail.com>
2019-04-26 14:49:31 +0700

    Can select object in world space and show it in focal space, can share material

--- SparkAR/VirtualGallery/scripts/script.js
+++ SparkAR/VirtualGallery/scripts/script.js
@@ -11,6 +11,7 @@ const TouchGestures = require('TouchGestures')
 const Diagnostics = require('Diagnostics');
 var Patches = require('Patches');
 const Time = require('Time');
+const Materials = require('Materials');
 
 var origin = Scene.root.find('origin')
 var planeTracker = Scene.root.find('planeTracker0');
@@ -53,8 +54,29 @@ function startScene() {
 	}
 }
 
-/*
+// Test move obj on tapping point
+const testObj = Scene.root.find("square_plane99");
+const displayImg = Scene.root.find("displayImage");
+const displayImgModel = Scene.root.find("displayImageModel");
 
+Patches.setBooleanValue("isShowDisplayImage", false);
+Patches.setBooleanValue("isShowSampleCanvas", true);
+TouchGestures.onTap(testObj).subscribe(function(gesture) {
+
+	// Show display image
+	Patches.setBooleanValue("isShowDisplayImage", true);
+
+	// Apply material
+	const material = Materials.get("imageMat3");
+	displayImgModel.material = material;
+});
+
+TouchGestures.onTap(displayImg).subscribe(function(gesture) {
+
+	Patches.setBooleanValue("isShowDisplayImage", false);
+});
+
+/*
 TouchGestures.onTap().subscribe(function(gesture) {
 	planeTracker.trackPoint(gesture.location);
 });
@@ -64,18 +86,18 @@ TouchGestures.onPan(planeTracker).subscribe(function(gesture) {
 });
 
 TouchGestures.onPinch().subscribe(function(gesture) {
-	var lastScaleX = origin.transform.scaleX.lastValue;
+	var lastScaleX = origin.transform.scaleX.pinLastValue();
 	origin.transform.scaleX = Reactive.mul(lastScaleX, gesture.scale);
 
-	var lastScaleY = origin.transform.scaleY.lastValue;
+	var lastScaleY = origin.transform.scaleY.pinLastValue();
 	origin.transform.scaleY = Reactive.mul(lastScaleY, gesture.scale);
 
-	var lastScaleZ = origin.transform.scaleZ.lastValue;
+	var lastScaleZ = origin.transform.scaleZ.pinLastValue();
 	origin.transform.scaleZ = Reactive.mul(lastScaleZ, gesture.scale);
 });
 
 TouchGestures.onRotate(origin).subscribe(function(gesture) {
-  var lastRotationY = origin.transform.rotationY.lastValue;
+  var lastRotationY = origin.transform.rotationY.pinLastValue();
   origin.transform.rotationY = Reactive.add(lastRotationY, Reactive.mul(-1, gesture.rotation));
 });
 
