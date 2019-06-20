ee1127906423856111e527f1e860fe501d87d54d
adawat <prophecyx@hotmail.com>
2019-06-20 16:15:11 +0700

    Has the new cool hachimaki in gyoza theme

--- SparkAR/OishiEato/scripts/script.js
+++ SparkAR/OishiEato/scripts/script.js
@@ -545,10 +545,10 @@ function main() {
     initSwirlSandwich(backSwirl, backSandwichList, backSandwichMeshList, false);
     
     hideAllThemes();
-    //showGyoza();
+    showGyoza();
     //showSandwich();
     //showCrabstick();
-    showMeal();
+    //showMeal();
 
     handleEyeOpeningState(0, 0, function() { onEyeOpened(0, 0); }, function() { onEyeClosed(0, 0); });
     handleEyeOpeningState(0, 1, function() { onEyeOpened(0, 1); }, function() { onEyeClosed(0, 1); });    
